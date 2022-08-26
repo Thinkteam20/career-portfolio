ba23ce7e7cd9cc6bed790f3d66fe7e5a5fdbe808
Sangpil <tankteam20@gmail.com>
2022-08-26 23:02:10 +1000

    add fullpage base set up

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import styles from "./app.module.css";
+import FullPageLanding from "./fullpage.jsx";
+
+function App() {
+    return (
+        <div className={styles.app}>
+            <FullPageLanding />
+        </div>
+    );
+}
+
+export default App;
